5c18815a3752724fac81233f2dc9ef0e2d9e6b8b
crltsnch <crltsnch@myuax.com>
2023-02-26 10:41:50 +0100

    segunda funcion con asyncio

--- teoria.py
+++ teoria.py
@@ -1,8 +1,21 @@
-import asyncio   #para escribir concurrente
+import asyncio   # para escribir asíncrona
 
 
-async def main():     #Async convierte la función en subrutina
+async def main():     # Async convierte la función en subrutina
     print('Hola...')
-    await asyncio.sleep(1)    #espera 1 segundo antes de imprimir el siguiente print. Imprimirá '...Mundo!' después de 1 segundo.
+    await asyncio.sleep(1)    # espera 1 segundo antes de imprimir el siguiente print. Imprimirá '...Mundo!' después de 1 segundo.
     print('...Mundo!')
 
+asyncio.run(main())     # ejecutar una corrutina
+main()
+
+
+# El siguiente fragmento de código imprimirá 'hola' después de esperar 1 segundo
+# y luego imprimirá 'mundo' después de esperar otros 2 segundos
+
+import time    # debemos importar time
+
+async def decir_despues(retrasar, que):
+    await asyncio.sleep(retrasar)
+    print(que)
+    

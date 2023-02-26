cee41db1b326995e606fbf2bbc3fd0f898a6cb43
crltsnch <crltsnch@myuax.com>
2023-02-26 10:58:34 +0100

    funcion con create_task

--- teoria.py
+++ teoria.py
@@ -9,6 +9,7 @@ async def main():     # Async convierte la función en subrutina
 asyncio.run(main())     # ejecutar una corrutina
 
 
+
 # El siguiente fragmento de código imprimirá 'hola' después de esperar 1 segundo
 # y luego imprimirá 'mundo' después de esperar otros 2 segundos
 
@@ -19,6 +20,8 @@ async def decir_despues(retrasar, que):
     print(que)
 
 async def main():
+    # Strftime() devuelve una cadena que representa la fecha y hora especifica, formateada según la especificación que le demos
+    # como nosotros queremos la hora, usamos %X.
     print(f"Empieza a {time.strftime('%X')}")
 
     await decir_despues(1, 'Hola')
@@ -27,3 +30,19 @@ async def main():
     print(f"Acaba a {time.strftime('%X')}")
 
 asyncio.run(main())
+
+
+
+#Aqui modificamos el ejemplo anterior y ejecutamos dos corrutinas decir_despues concurrentemente
+
+async def main():
+    # create_task() para ejecutar corrutinas concurrentemente como asyncio Tasks.
+    task1 = asyncio.create_task(decir_despues(1, 'Hola'))       
+    task2 = asyncio.create_task(decir_despues(2, 'mundo'))
+
+    print(f"Empieza a {time.strftime('%X')}")
+
+    # espera hasta que se completen ambas tareas (debe tomar alrededor de 2 segundos)
+    await task1
+    await task2
+

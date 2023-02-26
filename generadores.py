44f12f01a9b1062c1053597e941ed07eea7cd4f3
crltsnch <crltsnch@myuax.com>
2023-02-26 13:20:42 +0100

    .

--- generadores.py
+++ generadores.py
@@ -16,3 +16,8 @@ print(al_cuadrado_generador)    #se genera un objeto generador
 # Para los valores de nuestra lista iteramos sobre el generador:
 for i in al_cuadrado_generador:
     print(i)
+
+'''La diferencia entre el ejemplo usando list compregensions y generators es que en el caso de los generadores, 
+los valores no están almacenados en memoria, sino que se van generando al vuelo. Esta es una de las principales ventajas de los generadores,
+ya que los elementos sólo son generados cuando se piden, lo que hace que sean mucho más eficientes en lo relativo a la memoria.
+'''

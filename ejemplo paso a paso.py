611f80f558f375a887a9cf4788d1bcacdd7c4feb
crltsnch <crltsnch@myuax.com>
2023-04-14 19:55:11 +0200

    cambios funcion  get uri

--- ejemplo paso a paso.py
+++ ejemplo paso a paso.py
@@ -9,6 +9,7 @@ import requests
 
 
 '''Función descargar las imágenes de una página HTML'''
+'''Cuando se encuentre una imgaen se parsará a la siguiente funcion y se devolverá el control durante una siguiente espera en el programa'''
 def get_images_scr_from_html(html_doc):
     '''Recupera todo el contenido de los atributos src de las etiquetas img'''
     soup = BeautifulSoup(html_doc, 'html.parser')    #analiza el conjunto de la página
@@ -17,29 +18,29 @@ def get_images_scr_from_html(html_doc):
         asyncio.sleep(0.001) #espera 1 milisegundo
 
 
-'''Ahora queremos cada URI de la imagen a descargar'''
+'''Ahora queremos cada URI de cada imagen a descargar'''
+'''Se trata de otro generador pero que  trabaja a partir de los resultados del anterior'''
 def get_uri_from_images_src(base_uri, images_src):
-    parsed_base = urlparse(base_uri)       #analiza la base del URL. El elemento HTML <base> especifica la dirección URL base que se utilizará para todas las direcciones URL relativas contenidas dentro de un documento. Sólo puede haber un elemento <base> en un documento.
-
+    parsed_base = urlparse(base_uri)       #analiza la base del URI. El elemento HTML <base> especifica la dirección URL base que se utilizará para todas las direcciones URL relativas contenidas dentro de un documento. Sólo puede haber un elemento <base> en un documento.
     for src in images_src:
-        parsed = urlparse(src)      #analiza el URL de la imagen
+        parsed = urlparse(src)      #analiza el URI de la imagen
         if parsed.netloc == '':
             path = parsed.path      
             if parsed.query:
                 path += '?' + parsed.query
-    
             elif path[0] != '/':
                 if parsed_base.path == '/':
                     path = '/' + path
-
                 else:
                     path = '/' + '/'.join(parsed_base.path.split('/')[:-1]) + '/' + path
                 
             yield parsed_base.scheme + '://' + parsed_base.netloc + path    #devolvemos cada resultado en el momento en el que llega
         
         else:
-            yield parsed.geturl()     # de nuevo devolvemos cada resultado en el momento en el que llega
-     
+            yield parsed.geturl()     #devolvemos la URI de la imagen
+
+        asyncio.sleep(0.001) #espera 1 milisegundo
+
 
 '''Función que descarga las imágenes y escribe el archivo haciéndolas asíncronas'''
 async def main(uri):

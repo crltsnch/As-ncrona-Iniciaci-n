bac672fe4ed71e93c5467e6f90c0816475403fe3
crltsnch <crltsnch@myuax.com>
2023-04-14 19:18:17 +0200

    funcion wget

--- ejemplo paso a paso.py
+++ ejemplo paso a paso.py
@@ -2,10 +2,14 @@
 # Para no tener que esperar a recolectar todos los datos antes de tratarlos utilizamos los generadores.
 
 from bs4 import BeautifulSoup  #BeautifulSoup nos permite extraer información de contenido en formato HTML o XML
+import aiohttp
+import asyncio
 
-def get_images_scr_from_html(html_doc):    #descargar las imagenes de una pagina HTML
+
+
+def get_images_scr_from_html(html_doc):    #descargar las imagenes de una página HTML
     '''Recupera todo el contenido de los atributos src de las etiquetas img'''
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    soup = BeautifulSoup(html_doc, 'html.parser')    #analiza el conjunto de la página
     return (img.get('src') for img in soup.find_all('img'))   #devuelve un generador poruqe hemos cambiado los corchetes por paréntesis como en generaodres.py
 
 
@@ -18,7 +22,7 @@ def get_uri_from_images_src(base_uri, images_src):
     for src in images_src:
         parsed = urlparse(src)      #analiza el URL de la imagen
         if parsed.netloc == '':
-            path = parsed.path      #
+            path = parsed.path      
             if parsed.query:
                 path += '?' + parsed.query
     
@@ -29,8 +33,29 @@ def get_uri_from_images_src(base_uri, images_src):
                 else:
                     path = '/' + '/'.join(parsed_base.path.split('/')[:-1]) + '/' + path
                 
-            yield parsed_base.scheme + '://' + parsed_base.netloc + path    #devolvemos cad aresultado en el momento en el que llega
-
+            yield parsed_base.scheme + '://' + parsed_base.netloc + path    #devolvemos cada resultado en el momento en el que llega
+        
+        else:
+            yield parsed.geturl()     # de nuevo devolvemos cada resultado en el momento en el que llega
      
 
-            
+'''Función que descarga las imágenes y escribe el archivo haciéndolas asíncronas'''
+async def main(uri):
+    async with aiohttp.ClientSession() as seession:
+        async with session.get(uri) as response:
+            if response.status != 200:
+                return None
+            if response.content_type.startswith('text/'):
+                return await response.text()
+            else:
+                return await response.read()
+
+async def wget(session, uri):
+    async with session.get(uri) as response:
+        if response.status != 200:
+            return None
+        if response.content_type.startswith('text/'):
+            return await response.text()
+        else:
+            return await response.read()
+    

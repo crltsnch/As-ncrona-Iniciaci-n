27ee14e052af0ec0d46acba4015b1ec6880123c1
crltsnch <crltsnch@myuax.com>
2023-04-15 12:24:12 +0200

    funcion get_images_src_from_html acabada

--- ejemplo paso a paso.py
+++ ejemplo paso a paso.py
@@ -8,7 +8,6 @@ import asyncio
 import sys
 
 
-
 '''Función descargar las imágenes de una página HTML'''
 '''Cuando se encuentre una imgaen se parsará a la siguiente funcion y se devolverá el control durante una siguiente espera en el programa'''
 async def get_images_scr_from_html(html_doc):
@@ -16,7 +15,8 @@ async def get_images_scr_from_html(html_doc):
     soup = BeautifulSoup(html_doc, 'html.parser')    #analiza el conjunto de la página
     for img in soup.find_all('img'):    #busca todas las etiquetas img
         yield img.get('src')    #devuelve cada resultado en el momento en el que llega
-        asyncio.sleep(0.001) #espera 1 milisegundo
+        await asyncio.sleep(0.001)    #espera 1 milisegundo
+
 
 
 '''Ahora queremos cada URI de cada imagen a descargar'''
@@ -45,12 +45,12 @@ async def get_uri_from_images_src(base_uri, images_src):
 
 '''Función wget'''
 async def wget(session, uri):
-    async with session.get(uri) as response:
-        if response.status != 200:
+    async with session.get(uri) as response:     #para hacer una petición al contenido de una URI
+        if response.status != 200:   #indica el estado de la respuesta, 200 es correcto
             return None
         if response.content_type.startswith('text/'):    #si el tipo de contenido es texto
             return await response.text()   #devuelve el contenido de la respuesta
-        else: # si no es texto
+        else:    #si no es texto
             return await response.read()   #esperamos a que se descargue el contenido de la respuesta
 
 
@@ -84,6 +84,7 @@ async def main():
     web_page_uri = 'http://www.formation-python.com/'
     async with aiohttp.ClientSession() as session:
         await get_images(session, web_page_uri)
+
 
 '''Función para escribir en un archivo'''
 def write_file(filename, content):
